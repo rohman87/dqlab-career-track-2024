58572fa231404f7fcaf13b8ae7f2fb8ddbc8daa1
nemo <me.nemozuki.g.1@gmail.com>
2024-02-18 11:35:00 +0700

    Update 39.harga-rumah-tangerang.py

--- Python for Data Professional Beginner/Part 2/39.harga-rumah-tangerang.py
+++ Python for Data Professional Beginner/Part 2/39.harga-rumah-tangerang.py
@@ -1,13 +1,17 @@
+import requests
+from contextlib import closing
+import csv
 # STEP 1: 
 # Baca file "harga_rumah.txt"
-file_harga_rumah = open("harga_rumah.txt", "r")
-data_harga_rumah = file_harga_rumah.readlines()
-file_harga_rumah.close()
+url = "https://storage.googleapis.com/dqlab-dataset/harga_rumah.txt"
+data_harga_rumah = []
+with closing(requests.get(url, stream=True)) as r:
+    f = (line.decode('utf-8') for line in r.iter_lines())
+    data_harga_rumah = [row for row in csv.reader(f)]
 # Buat list of dict dengan nama harga rumah
-key_harga_rumah = data_harga_rumah[0].replace("\n","").split(",")
+key_harga_rumah = data_harga_rumah[0]
 harga_rumah = []
-for baris in data_harga_rumah[1:]:
-	baris_harga_rumah = baris.replace("\n","").split(",")
+for baris_harga_rumah in data_harga_rumah[1:]:
 	dict_harga_rumah = dict()
 	for i in range(len(baris_harga_rumah)):
 		dict_harga_rumah[key_harga_rumah[i]] = baris_harga_rumah[i]
