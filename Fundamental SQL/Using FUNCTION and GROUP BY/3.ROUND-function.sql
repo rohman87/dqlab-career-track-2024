88d7253da6f4e3584f1001ffd41995041697236a
nemo <me.nemozuki.g.1@gmail.com>
2024-02-25 18:13:34 +0700

    Update 3.ROUND-function.sql

--- Fundamental SQL/Using FUNCTION and GROUP BY/3.ROUND-function.sql
+++ Fundamental SQL/Using FUNCTION and GROUP BY/3.ROUND-function.sql
@@ -1,1 +1,1 @@
-select StudentID, FirstName, LastName, round(Semester1, 1) as Semester1, round(Semester1, 0) as Semester2, MarkGrowth from students;
+select StudentID, FirstName, LastName, round(Semester1, 1) as Semester1, round(Semester2, 0) as Semester2, MarkGrowth from students;
