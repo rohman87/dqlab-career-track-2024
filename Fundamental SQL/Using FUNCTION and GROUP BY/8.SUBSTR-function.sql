e670bdbd24c2fb196f843db3308f1f27652bffc5
nemo <me.nemozuki.g.1@gmail.com>
2024-02-25 18:18:04 +0700

    Update 8.SUBSTR-function.sql

--- Fundamental SQL/Using FUNCTION and GROUP BY/8.SUBSTR-function.sql
+++ Fundamental SQL/Using FUNCTION and GROUP BY/8.SUBSTR-function.sql
@@ -1,1 +1,31 @@
+Fungsi Text - SUBSTR( )
+Fungsi Text SUBSTR()
+
+Syntax: 
+
+SELECT SUBSTR(columnName, Start Index, Number of string to be extract)
+FROM TableName; 
+Keterangan:
+
+     columnName --> nama kolom yang akan dicari substring-nya
+
+     Start Index --> indeks dari text yang dimiliki (dimulai dari 1)
+
+     Number of string to be extract --> jumlah karakter atau beberapa karakter yang akan diambil.
+
+ 
+
+Contoh: 
+
+
+
+Tugas:
+Ketiklah contoh yang diberikan pada baris di atas pada live code editor.
+
+Output: 
+
+
+
+Pada contoh ini bisa dilihat aku mengambil substring mulai pada karakter ke dua hingga karakter ke empat.
+  
 select StudentID, substr(FirstName, 2, 3) as Initial from students;
