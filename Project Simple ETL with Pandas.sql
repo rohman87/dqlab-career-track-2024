7b5482678ed95aa840aaf8270ae39fe16b8cf340
nemo <me.nemozuki.g.1@gmail.com>
2024-02-26 13:34:16 +0700

    Update and rename Project Simple ETL with Pandas.txt to Project Simple ETL with Pandas.sql

--- Project Simple ETL with Pandas.sql
+++ Project Simple ETL with Pandas.sql
@@ -1,11 +1,9 @@
 Extract
 Extract merupakan proses meng-ekstraksi data dari sumber, sumber data ini bisa berupa relational data (SQL) atau tabel, nonrelational (NoSQL) maupun yang lainnya.
-
 Tugas Anda adalah baca terlebih dahulu dataset ini sebagai CSV agar nantinya bisa diolah. Gunakan live code editor untuk menampilkan dataset.
-
 File tersebut bisa diakses melalui URL: https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv.
 
-```sql
+sql
 import pandas as pd
 df_participant = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
-```
+
